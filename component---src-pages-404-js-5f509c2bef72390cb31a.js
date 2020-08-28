df4e50f22ee694d21fd88b8c49bdcfeaf6136839
shartzzz <funnydist@gmail.com>
2020-08-29 00:42:19 +0300

    Updates

---
+++ component---src-pages-404-js-5f509c2bef72390cb31a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("Bl7J"),c=t("vrFN");n.default=function(){return l.a.createElement(r.a,null,l.a.createElement(c.a,{title:"404: Страница не найдена"}),l.a.createElement("h1",null,"Страница не найдена"),l.a.createElement("p",null,"Просто вернитесь на гравную страницу"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-5f509c2bef72390cb31a.js.map
--- component---src-pages-course-js-4f59f69ab57b74fe11e2.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{Wb7o:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-course-js-4f59f69ab57b74fe11e2.js.map
